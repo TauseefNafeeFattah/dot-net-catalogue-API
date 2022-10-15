9b5b04c1adfee755a3807bb28d6cc189412d978e
TauseefNafeeFattah <tauseefnafee@gmail.com>
2022-10-14 20:21:55 -0600

    finished catalogue API

---
+++ Catalogue/Catalogue.Api/Repositories/IItemsRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Catalogue.Api.Entities;
+using System.Threading.Tasks;
+
+namespace Catalogue.Api.Repositories
+{
+    public interface IItemsRepository
+    {
+        Task<Item> GetItemAsync(Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
+    }
+}
